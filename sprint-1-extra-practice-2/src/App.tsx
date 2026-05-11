aee0b4a1c7685ef6cc2742af42c5f487f8615032
MarinaArmenovnaDev <awetisyanmarina@gmail.com>
2026-05-11 16:57:59 +0300

    first commit

---
+++ sprint-1-extra-practice-2/src/App.tsx
@@ -0,0 +1,32 @@
+import {useState} from 'react';
+import './App.css';
+import {Todolist} from './Todolist';
+
+export type FilterValuesType = "all" | "active" | "completed";
+
+export const App = ()=> {
+  let [tasks, setTasks] = useState([
+    {id: 1, title: "HTML&CSS", isDone: true},
+    {id: 2, title: "JS", isDone: true},
+    {id: 3, title: "ReactJS", isDone: false},
+    {id: 4, title: "Rest API", isDone: false},
+    {id: 5, title: "GraphQL", isDone: false},
+  ]);
+
+  function removeTask(id: number) {
+    let filteredTasks = tasks.filter(t => t.id != id);
+    setTasks(filteredTasks);
+  }
+  const deleteAllTasks = () => {
+    setTasks([])
+  }
+  return (
+    <div className="App">
+      <Todolist title="What to learn"
+                tasks={tasks}
+                removeTask={removeTask}
+                deleteAllTasks={deleteAllTasks}
+      />
+    </div>
+  );
+}
