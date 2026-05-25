8fbff479666a2a9d4abf598a1ab77d5e1fa31d35
MarinaArmenovnaDev <awetisyanmarina@gmail.com>
2026-05-25 20:43:17 +0300

    bankomat

---
+++ sprint-1-dop-3/src/City.tsx
@@ -0,0 +1,31 @@
+import {CurrentBankomat} from "./CurrentBankomat";
+import {MoneyType} from "./App";
+import styled from "styled-components";
+
+type CityPropsType = {
+    data: MoneyType[] //встречаем денюжки
+}
+
+export const City = ({data}: CityPropsType) => {
+// с деструктуризацией пожалуйста
+
+    const mappedMoney = data.map((el: MoneyType) => (
+        <CurrentBankomat
+            key={el.id}
+            money={el}
+        />
+    ))
+
+    return (
+        <Wrapper>
+            {mappedMoney}
+        </Wrapper>
+    );
+};
+
+const Wrapper = styled.div`
+  justify-content: center;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+`
---
+++ sprint-1-dop-3/src/CurrentBankomat.tsx
@@ -0,0 +1,68 @@
+import {MoneyType} from "./App";
+import styled from "styled-components";
+
+type CurrentBankomatPropsType = {
+    money: MoneyType
+}
+
+export const CurrentBankomat = ({money}: CurrentBankomatPropsType) => {
+    // с деструктуризацией пожалуйста
+    return (
+        // <div></div>
+        // ВНАЧАЛЕ НАПИШЕМ СОВСЕМ НЕКРАСИВО
+        // props.money.banknote==='USD'
+        //     ? ЗЕЛЕНАЯ
+        //     : СИНЯЯ
+
+    <div>
+        {money.banknote === "USD"
+            ? <BanknoteGreen>
+                <Name>{money.banknote}</Name>
+                <Nominal>{money.nominal}</Nominal>
+            </BanknoteGreen>
+            : <BanknoteBlue>
+                <Name>{money.banknote}</Name>
+                <Nominal>{money.nominal}</Nominal>
+            </BanknoteBlue>
+        }
+    </div>
+
+
+)
+    ;
+};
+
+
+const BanknoteGreen = styled.div`
+    background-color: aquamarine;
+    width: 200px;
+    height: 100px;
+    margin: 10px;
+`
+
+const BanknoteBlue = styled.div`
+    background-color: lightskyblue;
+    width: 200px;
+    height: 100px;
+    margin: 10px;
+`
+//
+// const Banknote = styled.div`
+//     width: 200px;
+//     height: 100px;
+//     margin: 5px;
+// `
+
+
+const Name = styled.span`
+    display: flex;
+    justify-content: center;
+    font-size: 15px;
+`
+
+const Nominal = styled.span`
+    display: flex;
+    justify-content: center;
+    margin-top: 15px;
+    font-size: 45px;
+`
